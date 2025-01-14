790405dd0cc5eaad0ba8f2a642499d7b298d2c38
acschoo <a.c.schoo95@gmail.com>
2025-01-14 12:42:19 +0100

    add a few new baselayers

--- scripts/viewer.js
+++ scripts/viewer.js
@@ -4,6 +4,17 @@
 var projectionExtent = [-285401.9200000018, 71093.05539999902, 546906.944600001, 903401.9200000018];
 var projectProjection = new ol.proj.Projection({ code: 'EPSG:28992', units: 'm', extent: projectionExtent, origin: [-285401.9200000018, 903401.9200000018] });
 
+// Geldigheidsgebied van het tiling schema in RD-coördinaten:
+var projectionExtent = [-285401.92, 22598.08, 595401.9199999999, 903401.9199999999];
+var projection = new ol.proj.Projection({ code: 'EPSG:28992', units: 'm', extent: projectionExtent });
+// Resoluties (pixels per meter) van de zoomniveaus:
+var resolutions = [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420, 0.210];
+var size = ol.extent.getWidth(projectionExtent) / 256;
+// Er zijn 15 (0 tot 14) zoomniveaus beschikbaar van de WMTS-service voor de BRT-Achtergrondkaart:
+var matrixIds = new Array(15);
+for (var z = 0; z < 15; ++z) {
+    matrixIds[z] = 'EPSG:28992:' + z;
+}
 
 // Define the view with the correct projection
 var view = new ol.View({
@@ -36,10 +47,63 @@ var Luchtfoto = new ol.layer.Tile({
     origin: [-285401.9200000018, 903401.9200000018],
 })
 
+
+var bgtLayer = new ol.layer.Tile({
+    title: 'BGT',
+    type: 'base',
+    visible: false,
+    source: new ol.source.WMTS({
+        attributions: 'Kaartgegevens: &copy; <a href="https://www.kadaster.nl">Kadaster</a>',
+        url: 'https://service.pdok.nl/lv/bgt/wmts/v1_0?',
+        layer: 'standaardvisualisatie',
+        matrixSet: 'EPSG:28992',
+        format: 'application/json',
+        projection: projection,
+        tileGrid: new ol.tilegrid.WMTS({
+            origin: ol.extent.getTopLeft(projectionExtent),
+            resolutions: resolutions,
+            matrixIds: matrixIds
+        }),
+        style: 'default',
+        wrapX: false
+    })
+})
+
+
+var brtLayer = new ol.layer.Tile({
+    title: 'BRT',
+    type: 'base',
+    visible: false,
+    source: new ol.source.WMTS({
+        attributions: 'Kaartgegevens: &copy; <a href="https://www.kadaster.nl">Kadaster</a>',
+        url: 'https://service.pdok.nl/brt/achtergrondkaart/wmts/v2_0?',
+        layer: 'standaard',
+        matrixSet: 'EPSG:28992',
+        format: 'image/png',
+        projection: projection,
+        tileGrid: new ol.tilegrid.WMTS({
+            origin: ol.extent.getTopLeft(projectionExtent),
+            resolutions: resolutions,
+            matrixIds: matrixIds
+        }),
+        style: 'default',
+        wrapX: false
+    })
+})
+
+
+var osmLayer = new ol.layer.Tile({
+    title: 'OpenStreetMap',
+    type: 'base',
+    visible: false,
+    source: new ol.source.OSM(),
+});
+
+
 // create basemap layer group
 var baseLayerGroup = new ol.layer.Group({
     title: "Base maps",
-    layers: [rdNewLayer, Luchtfoto]
+    layers: [rdNewLayer, Luchtfoto, bgtLayer, osmLayer, brtLayer, bagLayer]
 });
 
 
